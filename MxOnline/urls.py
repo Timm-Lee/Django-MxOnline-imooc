05f617f893bec96a476cadfb13a420534e3dbe34
macroot <macroot@macrootdeMacBook-Pro.local>
2017-05-06 12:02:17 +0800

    登录页面正常
    登录页面：
    * url
    * 静态文件
    
    主页：
    * 右上角 html 修改，正常显示登录/注册
    * 登录按钮，可以跳转到登录页面

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -7,6 +7,12 @@ import xadmin
 
 
 urlpatterns = [
+    # xadmin 后台管理页面
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
+
+    # 主页
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+
+    # 用户登录页面
+    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
